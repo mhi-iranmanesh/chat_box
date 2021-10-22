ac0fc5ccc071d53ce565ecca174cc3fa9e336a57
mohammad hassan iranmanesh <iranmanesh.mh@yaftar.ir>
2021-10-22 23:41:23 +0330

    return user with user and pass

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+          userId: 1,
+          username: 'mhi',
+          password: '123456',
+        },
+        {
+          userId: 2,
+          username: 'ali',
+          password: '123345',
+        },
+      ];
+    
+      async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+      }
+}
